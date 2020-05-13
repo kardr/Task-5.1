2b62022b6beed1bc59cde9eaa185947d5b5cfebd
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-13 15:32:29 +0300

    .

--- Task 5.1/Program.cs
+++ Task 5.1/Program.cs
@@ -8,8 +8,47 @@ namespace Task_5._1
 {
     class Program
     {
-        static void Main(string[] args)
+        static double f(double x)
         {
+            if ((x * x * x - 1)<0 || (x * x - 1)<0) throw new System.ArgumentException("Квадратный корень из отрицательного числа");
+            if ((x * x - 1)==0) throw new System.ArgumentException("Деление на ноль");
+            else return Math.Sqrt(x * x*x - 1) / Math.Sqrt(x * x - 1);
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("a=");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b=");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("h=");
+                double h = double.Parse(Console.ReadLine());
+                double x = a, y = 0;
+                while (x <= b)
+                    try
+                    {
+                        y = f(x);
+                        Console.WriteLine("x = {0} y = {1}", x, y);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("x = {0} error: {1}", x, ex.Message);
+                    }
+                    finally
+                    {
+                         x += h;
+                    }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода данных");
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
